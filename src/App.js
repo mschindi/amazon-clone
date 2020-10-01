dddaab7b47aa4d0a6527881933e1add6757a47e5
Michi Schindezow <michi@Gameboy.digitalnativesolutions>
2020-10-01 15:01:28 +0200

    Initialer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+//import logo from './logo.svg';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMI8hq5Pi10m-HZ4jBU8QbVauDa6GSEQs",
+  authDomain: "challenge-7afe9.firebaseapp.com",
+  databaseURL: "https://challenge-7afe9.firebaseio.com",
+  projectId: "challenge-7afe9",
+  storageBucket: "challenge-7afe9.appspot.com",
+  messagingSenderId: "1026014571880",
+  appId: "1:1026014571880:web:03c2a4ee25f309d0343add",
+};
